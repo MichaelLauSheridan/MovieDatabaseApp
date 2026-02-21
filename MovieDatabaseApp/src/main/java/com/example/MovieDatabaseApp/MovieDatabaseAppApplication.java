52a2e346051fb46cc7c3ea9f1c252c19ee26dfb9
Michael Lau <lau0@sheridancollege.ca>
2026-02-21 11:44:01 -0500

    Initialized project

---
+++ MovieDatabaseApp/src/main/java/com/example/MovieDatabaseApp/MovieDatabaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MovieDatabaseApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDatabaseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDatabaseAppApplication.class, args);
+	}
+
+}
---
+++ MovieDatabaseApp/src/test/java/com/example/MovieDatabaseApp/MovieDatabaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieDatabaseApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDatabaseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
