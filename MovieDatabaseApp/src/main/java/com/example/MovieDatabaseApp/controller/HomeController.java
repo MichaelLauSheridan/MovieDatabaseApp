46e8b1fb9eff1be0033754dcda49f153077f4ab4
Michael Lau <lau0@sheridancollege.ca>
2026-02-21 12:33:11 -0500

    Added home view with index and added it into homecontroller

--- MovieDatabaseApp/src/main/java/com/example/MovieDatabaseApp/controller/HomeController.java
+++ MovieDatabaseApp/src/main/java/com/example/MovieDatabaseApp/controller/HomeController.java
@@ -1,7 +1,23 @@
 package com.example.MovieDatabaseApp.controller;
 
+import com.example.MovieDatabaseApp.service.MovieService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    MovieService movieService;
+
+    public HomeController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/") // localhost:8080/
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to Movie Database App!");
+
+        return "index";
+    }
 }
